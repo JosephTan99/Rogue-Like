9c188af384f56664617216d0a5fd88f5bc77a38d
AustinKong <konghoitec@gmail.com>
2022-03-18 11:14:41 +0800

    Implemented temporary player movement
    Imported DoTween package, used to make a simple jump for player. Hold wasd to move. Still waiting for u to finish the tempo manager so for now i use an update loop to call Move()

--- Rogue/Assets/Scripts/MovementManager.cs
+++ Rogue/Assets/Scripts/MovementManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class MovementManager : MonoBehaviour
 {
@@ -18,9 +19,22 @@ public class MovementManager : MonoBehaviour
         instance = this;
     }
 
+    private float timer = 0;
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 1)
+        {
+            Move(new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")));
+            timer = 0;
+        }
+    }
 
     public void Move(Vector2 dir)
     {
-        gameObject.transform.position += (Vector3)dir;
+        transform.DOJump(transform.position + (Vector3)dir, 0.25f, 1, 0.5f);
+        if (dir.x < 0) GetComponent<SpriteRenderer>().flipX = true;
+        else if(dir.x > 0) GetComponent<SpriteRenderer>().flipX = false;
     }
+
 }
