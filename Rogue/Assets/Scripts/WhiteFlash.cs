14bd6bae7cac76f5594be4ec7b93e80e80126b42
AustinKong <konghoitec@gmail.com>
2022-03-20 09:18:54 +0800

    Flash script

---
+++ Rogue/Assets/Scripts/WhiteFlash.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhiteFlash : MonoBehaviour
+{
+    [SerializeField] private Material flashMaterial;
+
+    private Material originalMaterial;
+    private SpriteRenderer spriteRenderer;
+    private Coroutine flashCoroutine;
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalMaterial = spriteRenderer.material;
+    }
+
+    public void Flash()
+    {
+        if(flashCoroutine != null) StopCoroutine(flashCoroutine);
+        flashCoroutine = StartCoroutine(FlashCoroutine());
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Flash();
+        }
+    }
+
+    private IEnumerator FlashCoroutine()
+    {
+        spriteRenderer.material = flashMaterial;
+        yield return new WaitForSeconds(0.1f);
+        spriteRenderer.material = originalMaterial;
+        flashCoroutine = null;
+    }
+}
