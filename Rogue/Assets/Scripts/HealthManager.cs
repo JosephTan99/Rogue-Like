52a55e311c2e1b6b9259ecd9eb97ee0a236ab4d8
JosephTan99 <master99yi@gmail.com>
2022-03-22 21:17:45 +0800

    Health Manager Added

---
+++ Rogue/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// System that manages the player's health.
+/// </summary>
+public class HealthManager : MonoBehaviour
+{
+    private float health;
+
+
+    #region Getters & Setters
+    /// <summary>
+    /// Sets the current health of the player.
+    /// </summary>
+    /// <param name="newHealth"></param>
+    public void SetHealth(float newHealth)
+    {
+        health = newHealth;
+    }
+
+    /// <summary>
+    /// Returns the current player's health.
+    /// </summary>
+    /// <returns></returns>
+    public float GetHealth()
+    {
+        return health;
+    }
+    #endregion
+
+    /// <summary>
+    /// Reduces the health of the player according to the damage taken in parameter.
+    /// </summary>
+    /// <param name="damage"></param>
+    public void TakeDamage(float damage)
+    {
+        if (health - damage <= 0)
+        {
+            //Death to be added
+            return;
+        }
+        health -= damage;
+    }
+}
