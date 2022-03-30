2616b5961304167a417a81af81b9a93c07503397
JosephTan99 <master99yi@gmail.com>
2022-03-30 21:07:15 +0800

    Added projectile check

--- Rogue/Assets/Scripts/AIScripts/BaseAI.cs
+++ Rogue/Assets/Scripts/AIScripts/BaseAI.cs
@@ -6,6 +6,8 @@ using DG.Tweening;
 public class BaseAI : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
+
+    private bool canMove = true;
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -15,11 +17,28 @@ public class BaseAI : MonoBehaviour
 
     public void Move(Vector2 velocity)
     {
-        GridManager.instance.SetGridTile(new GameTile(TileType.Empty, null), Vector2Int.RoundToInt(transform.position));
-        GridManager.instance.SetGridTile(new GameTile(TileType.Enemy, gameObject), Vector2Int.RoundToInt(transform.position + (Vector3)velocity));
-        transform.DOJump(transform.position + (Vector3)velocity, 0.25f, 1, 0.5f);
-        if (velocity.x < 0) spriteRenderer.flipX = true;
-        else if (velocity.x > 0) spriteRenderer.flipX = false;
+        if (!canMove)
+        {
+            canMove = true;
+        }
+        else
+        {
+            GridManager.instance.SetGridTile(new GameTile(TileType.Empty, null), Vector2Int.RoundToInt(transform.position));
+            GridManager.instance.SetGridTile(new GameTile(TileType.Enemy, gameObject), Vector2Int.RoundToInt(transform.position + (Vector3)velocity));
+            transform.DOJump(transform.position + (Vector3)velocity, 0.25f, 1, 0.5f);
+            if (velocity.x < 0) spriteRenderer.flipX = true;
+            else if (velocity.x > 0) spriteRenderer.flipX = false;
+        }
+    }
+
+    public virtual Vector2Int MoveTile()
+    {
+        return Vector2Int.zero;
+    }
+
+    public void LockMove()
+    {
+        canMove = false;
     }
 
     public virtual void TickUpdateA() { }
